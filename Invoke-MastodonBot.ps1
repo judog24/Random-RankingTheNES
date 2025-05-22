b9efc0d962b568857f80e4e7165b32f2a2bc2f4f
judog24 <judog24@gmail.com>
2025-05-21 21:17:03 -0700

    Downloads latest CSV

--- Invoke-MastodonBot.ps1
+++ Invoke-MastodonBot.ps1
@@ -1,4 +1,4 @@
-Import-Module .\Get-RandomGame.psm1
+Import-Module .\Get-RandomGame.psm1 -Force
 
 if (test-path -Path '.\config.psd1') {
     $Config = Import-PowerShellDataFile '.\config.psd1'
@@ -8,6 +8,9 @@ if (test-path -Path '.\config.psd1') {
         AccessToken = ""
     }
 }
+
+$rankingURL = "https://raw.githubusercontent.com/vNakamura/8bitnintendo-science/refs/heads/main/_data/list.csv"
+Receive-GamesCSV -ListURL $rankingURL
 
 $game = Get-RandomGame
 $message = Format-Message -SelectedGame $game
@@ -23,4 +26,9 @@ $body = @{
     status = $message
 }
 
-Invoke-RestMethod -Uri $url -Method Post -Headers $headers -Body $body
+try {
+    Invoke-RestMethod -Uri $url -Method Post -Headers $headers -Body $body
+}
+catch {
+    "Unable to post message"
+}
